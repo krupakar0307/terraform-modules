12363dfb52737d9a5f6ea1eae15517f3549e1a32
krupakar <krupakarreddy.yasa@gmail.com>
2023-11-24 22:45:20 +0530

    simple ec2 with nginx

---
+++ simple_ec_instance/README.md
@@ -0,0 +1,6 @@
+* * * Simple EC2 Instance create
+* * Creates EC2 instance along with security groups and instance contsists of nginx server installed.
+* This folder creates a resources mentioned above.
+Clone code and do `terraform init`, `terraform plan` and followed by  `terraform apply`
+
+It's a simple EC2 terraform code that installs nginx and opens security group inbound ports to access nginx homepage.
---
+++ simple_ec_instance/vars.tf
@@ -0,0 +1,24 @@
+variable "ami_value" {
+    default = "ami-0a11b74e4c74e3790"
+}
+
+variable "instance_size" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+    default = "my_key"
+}
+
+variable "tags" {
+    type = map(string)
+    default = {
+        Name = "my_nginx_server"
+        state = "Running"
+        description = "nginx server runs in it"
+    } 
+}
+
+variable "security_group_name" {
+    default = "allow_tls"
+}
